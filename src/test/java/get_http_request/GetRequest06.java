8f479522d1ccb87fe13581cd16d93b55f3cf9b1a
taceddin öz <taceddin95@gmail.com>
2022-03-03 00:23:26 +0300

    api 2.test sonu

---
+++ src/test/java/get_http_request/GetRequest06.java
@@ -0,0 +1,48 @@
+package get_http_request;
+
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest06 {
+
+    @Test
+    public void test01() {
+
+        /*
+        https://restful-booker.herokuapp.com/booking/5 url’ine
+        accept type’i “application/json” olan GET request’i yolladigimda
+        gelen response’un
+        status kodunun 200
+        ve content type’inin “application/json”
+        ve "firstname": "Susan"
+        ve "totalprice": 958,
+        ve "checkin": "2018-12-06",oldugunu test edin
+         */
+
+
+        String url = "https://restful-booker.herokuapp.com/booking/5";
+
+        Response response = given().when().get(url);
+        response.prettyPrint();
+
+        //headers
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json");
+
+        //body
+        response.
+                then().
+                assertThat().
+                body("firstname", Matchers.equalTo("Susan"),
+                        "totalprice", Matchers.equalTo(958),
+                        "bookingdates.checkin", Matchers.equalTo("2018-12-06"));
+
+
+    }
+}
